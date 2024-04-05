f7cf201c56a0a3bd7606c094194b60efd63845a3
Berta Nicolau <berta.nico20@gmail.com>
2024-04-05 17:12:57 +0200

    Implement vertical layout for GetResponse form block

--- src/getresponse-form/edit.js
+++ src/getresponse-form/edit.js
@@ -43,98 +43,130 @@ export default function Edit({ attributes, setAttributes }) {
 	const { uniqueId } = 'attributes';
 
 	useEffect(() => {
-        if (!uniqueId) {
-            // Asigna un ID único basado en instanceId
-            setAttributes({ uniqueId: `getresponse-form-${instanceId}` });
-        }
-    }, [uniqueId, setAttributes]);
-    // Extracting attributes and setAttributes function from props
+		if (!uniqueId) {
+			// Asigna un ID único basado en instanceId
+			setAttributes({ uniqueId: `getresponse-form-${instanceId}` });
+		}
+	}, [uniqueId, setAttributes]);
+	// Extracting attributes and setAttributes function from props
 	// preventRedirect, confirmationMessage, destinationUrl		
-    const { campaignToken, inputLabel, buttonLabel, destinationUrl, termsAndConditionsText } = attributes;
+	const { campaignToken, inputLabel, buttonLabel, destinationUrl, termsAndConditionsText, hasRowAlign, RowAlign } = attributes;
 
-    // Handler functions for attribute changes
-    const onChangeCampaignToken = (value) => {
-        setAttributes( {
-			campaignToken: value === undefined ? 'none' : value, 
+	// Handler functions for attribute changes
+	const onChangeCampaignToken = (value) => {
+		setAttributes({
+			campaignToken: value === undefined ? 'none' : value,
 		});
-    };
+	};
 	const onChangeInputLabel = (newInputLabel) => {
-        setAttributes( {
-			inputLabel: newInputLabel, 
+		setAttributes({
+			inputLabel: newInputLabel,
 		});
-    };
+	};
 
 	const onChangeButtonLabel = (newButtonLabel) => {
-        setAttributes( {
-			buttonLabel: newButtonLabel, 
+		setAttributes({
+			buttonLabel: newButtonLabel,
 		});
-    };
-	
+	};
+
 	const onChangeDestinationUrl = (newDestinationUrl) => {
-        setAttributes( {
-			destinationUrl: newDestinationUrl, 
+		setAttributes({
+			destinationUrl: newDestinationUrl,
 		});
-    };
+	};
 	const onChangeTermsAndConditionsText = (newTermsAndConditionsText) => {
-        setAttributes( {
-			termsAndConditionsText: newTermsAndConditionsText, 
+		setAttributes({
+			termsAndConditionsText: newTermsAndConditionsText,
 		});
-    };
+	};
+	const onChangeRowAlign = (newRowAlign) => {
+		setAttributes({
+			hasRowAlign: newRowAlign,
+		});
+	};
 
-    // Add similar handler functions for other attributes like buttonLabel, preventRedirect, etc.
+	// Add similar handler functions for other attributes like buttonLabel, preventRedirect, etc.
 
-    return (
+	return (
 		<div {...useBlockProps()}>
 			<InspectorControls>
-                <PanelBody 
+				<PanelBody
 					title={__("Configuración del Bloque", "getresponse-form-block")}
 					initialOpen={true}
 				>
+
 					<PanelRow>
-						<TextControl
-							label={__("Token de Campaña", "getresponse-form-block")}
-							value={ campaignToken }
-							onChange={ onChangeCampaignToken }
+						<ToggleControl
+							label={__("Alineación", "getresponse-form-block")}
+							help={
+								hasRowAlign
+									? 'Has row align.'
+									: 'Has column align.'
+							}
+							checked={hasRowAlign}
+							onChange={() => setAttributes({ hasRowAlign: !hasRowAlign })}
 						/>
 					</PanelRow>
 					<PanelRow>
 						<TextControl
 							label={__("Texto del input", "getresponse-form-block")}
-							value={ inputLabel }
-							onChange={ onChangeInputLabel }
+							value={inputLabel}
+							onChange={onChangeInputLabel}
 						/>
 					</PanelRow>
 					<PanelRow>
 						<TextControl
 							label={__("Texto del botón", "getresponse-form-block")}
-							value={ buttonLabel }
-							onChange={ onChangeButtonLabel }
+							value={buttonLabel}
+							onChange={onChangeButtonLabel}
 						/>
 					</PanelRow>
 				</PanelBody>
-				<PanelBody 
+				<PanelBody
 					title={__("Confirmación", "getresponse-form-block")}
 					initialOpen={false}
 				>
-				
+					<PanelRow>
+						<TextControl
+							label={__("Token de Campaña", "getresponse-form-block")}
+							value={campaignToken}
+							onChange={onChangeCampaignToken}
+						/>
+					</PanelRow>
 					<PanelRow>
 						<TextControl
 							label={__("Términos y política de privacidad", "getresponse-form-block")}
-							value={ termsAndConditionsText }
-							onChange={ onChangeTermsAndConditionsText }
+							value={termsAndConditionsText}
+							onChange={onChangeTermsAndConditionsText}
 						/>
 					</PanelRow>
 					<PanelRow>
 						<TextControl
 							label={__("URL de destino", "getresponse-form-block")}
-							value={ destinationUrl }
-							onChange={ onChangeDestinationUrl }
+							value={destinationUrl}
+							onChange={onChangeDestinationUrl}
 						/>
 					</PanelRow>
 				</PanelBody>
-            </InspectorControls>
-			<p>Hello World { buttonLabel} and { campaignToken}  </p>
-			
+			</InspectorControls>
+			<div class="lead-mail-form" data-rowalign={hasRowAlign} id={uniqueId}>
+				<div class="form-group form-group__first">
+					{/* Email input field (required) */}
+					<div class="form-field form-field__email alone-input-email hidden-label__field">
+						{inputLabel}:
+					</div>
+
+					{/* Submit button */}
+					<div class="form-field form-field__terms-conditions">
+						▢ {termsAndConditionsText}
+					</div>
+					<div class="form-field form-field__submit-button">
+						<input class="g-recaptcha" data-callback='onSubmit' data-action='submit' data-id={uniqueId} data-sitekey="6LcFAcMUAAAAAJLFEF2PLqrNTh88qBoWzBQbJ4dP" type="submit" value={buttonLabel || 'Enviar'} />
+					</div>
+				</div>
+			</div>
+
 		</div>
-    );
+	);
 }
