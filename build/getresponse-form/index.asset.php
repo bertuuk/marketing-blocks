7278199d587965d5e221686dbda79b65ea0ad45f
Bertuuk <22979999+bertuuk@users.noreply.github.com>
2025-07-29 23:00:51 +0200

    Add REST API endpoints for form validation and reCAPTCHA

--- build/getresponse-form/index.asset.php
+++ build/getresponse-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '6e599ec32306ab4dfc4c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'f30e404b4d810c862125');
